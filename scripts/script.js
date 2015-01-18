550d4b4e46a9c1ee13a37834e616bfeac041d3c6
Ethan Arterberry <ethanarterberry@gmail.com>
2015-01-18 04:38:56 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 // Made for YogaScript by Douglas Bumby
 var count = parseInt(localStorage.getItem("yogapantcount"))||0;
+var yogaCount;
+
 
 count = 2;
 
 yogaCount = function() {
   count = count++;
   document.getElementById("yoga-tally").innerHTML = count;
+  localStorage.setItem("yogapantcount", count);
   return 0;
 };
